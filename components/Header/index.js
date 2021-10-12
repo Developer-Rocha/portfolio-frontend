abc2f219004ed47674e5723470363b78b10c0786
developer-rocha <fabricio.rocha@eae.pt>
2021-10-13 00:35:19 +0100

    add label translation for menu

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { MobileNavToggle, HeaderWraper } from "./styles";
+import { useTranslation } from "next-i18next";
 
 function Header(props) {
+	const { t } = useTranslation("common");
+
 	return (
 		<>
 			<MobileNavToggle className="bi bi-list mobile-nav-toggle d-xl-none" />
@@ -10,12 +13,12 @@ function Header(props) {
 					<ul>
 						<li>
 							<a href="#hero" className="nav-link scrollto active">
-								<i className="bx bx-home"></i> <span>Home</span>
+								<i className="bx bx-home"></i> <span>{t("home")}</span>
 							</a>
 						</li>
 						<li>
 							<a href="#about" className="nav-link scrollto">
-								<i className="bx bx-user"></i> <span>About</span>
+								<i className="bx bx-user"></i> <span>{t("about")}</span>
 							</a>
 						</li>
 						<li>
@@ -25,17 +28,18 @@ function Header(props) {
 						</li>
 						<li>
 							<a href="#portfolio" className="nav-link scrollto">
-								<i className="bx bx-book-content"></i> <span>Portfolio</span>
+								<i className="bx bx-book-content"></i>{" "}
+								<span>{t("portfolio")}</span>
 							</a>
 						</li>
 						<li>
 							<a href="#services" className="nav-link scrollto">
-								<i className="bx bx-server"></i> <span>Services</span>
+								<i className="bx bx-server"></i> <span>{t("services")}</span>
 							</a>
 						</li>
 						<li>
 							<a href="#contact" className="nav-link scrollto">
-								<i className="bx bx-envelope"></i> <span>Contact</span>
+								<i className="bx bx-envelope"></i> <span>{t("contact")}</span>
 							</a>
 						</li>
 					</ul>
